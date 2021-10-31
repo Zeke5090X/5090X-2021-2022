28414fe01911b9edc1e10917a0e194861cd0995d
Zeke5090X <zekeloyer2004@gmail.com>
2021-10-30 19:59:01 -0700

    Updated drive.cpp
    added drive orientation toggle
    cleaned up drive.cpp
    added comments

--- src/drive.cpp
+++ src/drive.cpp
@@ -20,33 +20,58 @@ void rightdrive(int rds){
     r3.move_velocity(rds);
 }
 //groups together the right drive motors
+//this doesnt work. will revisit
 
 void drive(){
-   right_y = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
-   left_y = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-		l1.move(-left_y);
-		l2.move(left_y);
-		l3.move(-left_y);
-		r1.move(right_y);
-		r2.move(right_y);
-		r3.move(-right_y);
-    if(master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)){
+    bool reverse_drive = false;
+   if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)){
+       reverse_drive = !reverse_drive;
+   }
+   //toggles which side of the robot is the front
+   if(reverse_drive == true){
+       if(master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)){
         l1.move_velocity(-70);
 		l2.move_velocity(70);
 		l3.move_velocity(-70);
 		r1.move_velocity(70);
 		r2.move_velocity(70);
 		r3.move_velocity(-70);
-    }if(master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)){
+    }if(master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)){
         l1.move_velocity(70);
 		l2.move_velocity(-70);
 		l3.move_velocity(70);
 		r1.move_velocity(-70);
 		r2.move_velocity(-70);
 		r3.move_velocity(70);
-    }
-    //doesn't work. will revisit
-    //leftdrive(127);
-    //rightdrive(127);
-    //printf("%d", lds);
+    }right_y = -master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+   left_y = -master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+   }
+   //4bar is the front
+   else{
+        right_y = master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+        left_y = master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+        if(master.get_digital(pros::E_CONTROLLER_DIGITAL_UP)){
+            l1.move_velocity(-70);
+		    l2.move_velocity(70);
+		    l3.move_velocity(-70);
+		    r1.move_velocity(70);
+		    r2.move_velocity(70);
+		    r3.move_velocity(-70);
+        }if(master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN)){
+            l1.move_velocity(70);
+		    l2.move_velocity(-70);
+		    l3.move_velocity(70);
+		    r1.move_velocity(-70);
+		    r2.move_velocity(-70);
+		    r3.move_velocity(70);
+        }
+   }
+    //lift is in front
+
+    l1.move(-left_y);
+	l2.move(left_y);
+	l3.move(-left_y);
+	r1.move(right_y);
+	r2.move(right_y);
+	r3.move(-right_y);
 }
